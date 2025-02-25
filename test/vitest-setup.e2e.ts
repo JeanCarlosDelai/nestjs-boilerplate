9cfc935fce7e49b03d9ca09d6acba2cf66ddcc72
Jean Carlos Delai <112594276+JeanCarlosDelai@users.noreply.github.com>
2025-02-25 10:38:39 -0300

    chore(): update vitest config files

--- test/vitest-setup.e2e.ts
+++ test/vitest-setup.e2e.ts
@@ -1,17 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import { AllExceptionsFilter } from 'src/shared/filters/AllException.filter';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from 'src/app.module';
+import { AllExceptionFilter } from '@src/shared/infra/filter/all-exeption.filter';
+import { AppLogger } from '@src/shared/infra/logger/service/app-logger.service';
 
 let app: INestApplication;
 
 export const setupAppTest = async (): Promise<INestApplication> => {
   const moduleFixture: TestingModule = await Test.createTestingModule({
-    imports: [AppModule],
+    imports: [AppModule]
   }).compile();
 
   app = moduleFixture.createNestApplication();
-  app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const appLogger = new AppLogger();
+  app.useGlobalFilters(new AllExceptionFilter(appLogger));
 
   await app.init();
   return app;
